c2a6a9b3929f6f44b455f69b902ed6fdb6969a45
Ignacio Cavicchioli <104076568+Icavicchioli@users.noreply.github.com>
2025-12-26 14:37:14 -0300

    v1

--- monografia/simulink/clonado_control.m
+++ monografia/simulink/clonado_control.m
@@ -1,6 +1,8 @@
 %% ============================
 %  ARMADO DE DATASET
 % =============================
+load('clon_controlador_dataset.mat')
+%%
 
 % Extraer señales desde Simulink
 y = out.Y(:);        % salida de la planta
@@ -92,5 +94,90 @@ grid on;
 title('Error de predicción');
 
 %% ============================
+
+gensim(net);
+
+%% otra sin referencia
+
+%% ============================
+%  ARMADO DE DATASET
+% ============================
+clc;
+clear all;
+load('clon_controlador_dataset_2.mat')
+
+% Extraer señales desde Simulink
+y = out.Y(:);        % salida de la planta
+e = out.error(:);    % error
+c = out.C(:);        % acción de control (PID)
+
+N = length(y);
+
+%% Parámetros del modelo
+na = 10;                 % retardos de salida
+n0 = na + 1;
+Ns = N - na;
+
+% Inicialización
+X = zeros(na + 1, Ns);   % [y(k-1..k-10); e(k)]
+T = zeros(1, Ns);        % C(k)
+
+for k = n0:N
+    X(1:na, k-na) = y(k-1:-1:k-na);  % salidas pasadas
+    X(na+1, k-na) = e(k);            % error actual
+    T(k-na)       = c(k);            % salida objetivo
+end
+
+%% ============================
+%  DIVISIÓN TRAIN / TEST
+% ============================
+
+trainRatio = 0.70;
+Ntrain = floor(trainRatio * Ns);
+
+idxTrain = 1:Ntrain;
+idxTest  = (Ntrain+1):Ns;
+
+%% ============================
+%  CREACIÓN DE LA RED
+% ============================
+
+numNeuronas = 20;
+net = fitnet(numNeuronas, 'trainlm');
+
+% Funciones de activación
+net.layers{1}.transferFcn = 'tansig';
+net.layers{2}.transferFcn = 'purelin';
+
+% Función de costo
+net.performFcn = 'mse';
+
+% División explícita SIN validación
+net.divideFcn = 'divideind';
+net.divideParam.trainInd = idxTrain;
+net.divideParam.valInd   = [];
+net.divideParam.testInd  = idxTest;
+
+%% ============================
+%  PARÁMETROS DE ENTRENAMIENTO
+% ============================
+
+epochs = 2000;
+
+net.trainParam.epochs   = epochs;
+net.trainParam.min_grad = 1e-9;
+net.trainParam.mu_max   = 1e12;
+net.trainParam.time     = inf;
+
+%% ============================
+%  ENTRENAMIENTO
+% ============================
+
+[net, tr] = train(net, X, T);
+
+
+%% ============================
+%  GENERAR BLOQUE SIMULINK
+% ============================
 
 gensim(net);
